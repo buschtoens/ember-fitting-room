623dc6f85401714406b617a0e854d4bea0de882e
Jan Buschtöns <jan@buschtoens.me>
2018-03-02 07:30:15 +0100

    chore(deps): upgrade to ember-cli@3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,52 @@
 module.exports = {
   root: true,
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
   plugins: [
-    "prettier"
+    'prettier',
+    'ember'
   ],
   extends: [
     'eslint:recommended',
+    'plugin:ember/recommended',
     'prettier'
   ],
   env: {
     browser: true
   },
   rules: {
-    "prettier/prettier": ["error", { singleQuote: true }]
-  }
+    'prettier/prettier': ['error', { singleQuote: true }]
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        'index.js',
+        'testem.js',
+        'ember-cli-build.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js'
+      ],
+      excludedFiles: [
+        'app/**',
+        'addon/**',
+        'tests/dummy/app/**'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      },
+      plugins: ['node'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+      })
+    }
+  ]
 };
